d3b0ff1a1c240138b3559e8294dc4aa060210c45
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 21:15:09 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,4 @@
 def reverse_each_word(string)
-  newString = string.split
-  reversedString = ""
-  newString.each do |word|
-    reversedString << word.reverse
-    reversedString << " "
-  end
-  reversedString
-end
-
-
-def reverse_each_word(string)
   string.split.collect{|word|
     word.reverse}.join(" ")
 end
